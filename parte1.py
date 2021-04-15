7d7d0fc390a7e81f254a0d9ca4c65474fb0a2bb3
VLx2000 <victor.antunes.2000@gmail.com>
2021-04-15 18:56:02 -0300

    +comentarios

--- parte1.py
+++ parte1.py
@@ -9,6 +9,7 @@ from sklearn.metrics import classification_report
 # X - Matriz de atributos y - vetor de classes
 X, y = load_wine(return_X_y=True)
 
+# Transformando nominais em binarios
 OneHotEncoder(X)
 
 # Separar em conjunto treinamento e testes
@@ -19,17 +20,20 @@ arvore = tree.DecisionTreeClassifier(criterion='entropy')
 # algoritmo de induçao
 arvore = arvore.fit(X_train, y_train)
 
+# Plota arvore
 fig = plt.figure(figsize=(10,10))
 fig = tree.plot_tree(arvore)
 
 # Classifica os dados
 previsto = arvore.predict(X_test)
 
+# Plota Matriz
 class_names = y
 title = "Matriz Confusão"
 disp = plot_confusion_matrix(arvore, X_test, y_test, display_labels=class_names, cmap=plt.cm.Blues)
 disp.ax_.set_title(title)
 plt.show()
 
+# Printa resultados
 print("Relatório")
 print(f"{classification_report(y_test, previsto)}")
