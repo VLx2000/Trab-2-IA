01931143d66502444e8ef8ddf0a893a11b39a4b8
VLx2000 <victor.antunes.2000@gmail.com>
2021-04-15 18:24:11 -0300

    comentarios

--- parte 2.py
+++ parte 2.py
@@ -3,22 +3,28 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 
+# X - Matriz de atributos y - vetor de classes
 X, y = load_diabetes(return_X_y=True)
 
+# Transformando nominais em binarios
 OneHotEncoder(X)
 
 soma_quadrados_distancias = []
+
+# Definindo intervalo para nº de grupos
 group_range = range(1, 15)
 
+# Aplicando algoritmo para valores no intervalo definido
 for i in group_range:
     kmeans = KMeans(n_clusters=i, init='random')
     kmeans.fit(X)
-
+    # Coletando valor do indice
     soma_quadrados_distancias.append(kmeans.inertia_)
 
 for j in range(1, 15):
     print(f"Grupo {j}: {soma_quadrados_distancias[j - 1]}")
 
+# Plotando o grafico
 plt.plot(group_range, soma_quadrados_distancias, 'bx-')
 plt.xlabel('group range')
 plt.ylabel('Soma dos Quadrados das Distâncias')
